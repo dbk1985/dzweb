0d1e0663c7cf79dec75b35a853443f713c374228
dlm <376958755@qq.com>
2015-09-13 18:24:52 +0800

    资源编译位置调整  tomcat插件支持

--- dzstage/src/main/java/com/dzeep/dzstage/controller/Login.java
+++ dzstage/src/main/java/com/dzeep/dzstage/controller/Login.java
@@ -6,5 +6,8 @@ import com.dzeep.dzframe.controller.Base;
  * Created by fighting on 15/9/9.
  */
 public class Login extends Base {
-
+    @Override
+    public String execute() throws Exception {
+        return SUCCESS;
+    }
 }
